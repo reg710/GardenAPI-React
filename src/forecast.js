c53edb18148a2629efb49004b899de9586178246
rcon <reg710@gmail.com>
2021-08-22 16:11:37 -0400

    Axios get request is working and saving an object to state

--- src/forecast.js
+++ src/forecast.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './forecast.css';
+import Axios from 'axios';
 
 
 // Not sure if this is the usual place to store this information
 const API_KEY = process.env.REACT_APP_API_KEY;
+
+const options = {
+    method: 'GET',
+    url: 'https://weatherapi-com.p.rapidapi.com/history.json',
+    params: {q: '15201', dt: '2021-08-16', lang: 'en'},
+    headers: {
+      'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
+      'x-rapidapi-key': `${API_KEY}`
+    }
+  };
 
 class Forecast extends React.Component {
     // function that allows me to set up state
@@ -20,8 +31,17 @@ class Forecast extends React.Component {
     }
 
     runAxios() {
+        Axios.request(options)
+        .then((response) => {
+            console.log(response.data);
+            this.setState({
+               weather_history: response.data.forecast.forecastday
+            });
+        }).catch(function (error) {
+            console.error(error);
+        });
 
-        console.log(API_KEY)
+        console.log(this.state.weather_history)
     }
 
     render() {
